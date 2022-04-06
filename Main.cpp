595073d7f797111cd812ac4ce25c744d7ae00567
Michael Nguyen <98675293+MikeyViet@users.noreply.github.com>
2022-04-06 23:01:20 +0000

    Initial commit

---
+++ Main.cpp
@@ -0,0 +1,15 @@
+/****************************************************************
+ *	Unit 3 Assignment - Multi-Dimensional Arrays
+ *	Student:		Michael Nguyen
+ *	Teacher: 		Tyson McMillan
+ *	Description:This program shows us how multi-dimensional arrays
+ *		work within C++. How to create, populate, and print the 
+ *		elements within the array.
+ *****************************************************************/
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  
+}
